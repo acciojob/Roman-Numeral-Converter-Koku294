ea78ca21b2ab895b44fde78fff2cfe69ea5f9ced
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-01-30 20:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,23 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+     let roman = '';
+    for (let i = 0; i <= 6; i++) {
+      const [letter, value] = obj[i];
+      
+      
+      while (num >= value) {
+        roman += letter;
+        num -= value;
+      }
+    }
 
-  //your code here
+    return roman;
+}
+
+// Testing with the input 36
+console.log(convertToRoman(36)); // Expected output: XXXVI
+  
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
