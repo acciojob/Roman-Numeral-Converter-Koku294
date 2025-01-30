7dcc8bbba31045080a2dd0899da6d314ff94cb08
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-01-30 20:12:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,17 +22,14 @@ function convertToRoman(num) {
     return roman;
 }
 
-// Testing with the input 36
-console.log(convertToRoman(36)); // Expected output: XXXVI
+
+console.log(convertToRoman(36)); 
   
 
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
 
 
 
 
-// do not edit below this line
+
 module.exports = convertToRoman
